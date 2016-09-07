cf81a70836b695561452cef47d6e9b1f7539152d
Nivekuh <kevin.hu1000@gmail.com>
2016-09-07 11:25:24 -0700

    Added files

---
+++ APLesson_01/ASCIIArt_01.java
@@ -0,0 +1,7 @@
+public class ASCIIArt_01
+{
+	public static void main(String[]args)
+	{
+		System.out.println("H   H\nH   H\nHHHHH\nH   H\nH   H");
+	}
+}
---
+++ APLesson_01/ASCIIArt_02.java
@@ -0,0 +1,7 @@
+public class ASCIIArt_02
+{
+	public static void main(String[]args)
+	{
+		System.out.println("EEEEE\nE\nEEEEE\nE\nEEEEE");
+	}
+}
