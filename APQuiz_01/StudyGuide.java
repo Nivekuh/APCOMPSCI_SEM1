5e8bdce1549bc942cb0bf546afceeb2510fdbbae
Nivekuh <kevin.hu1000@gmail.com>
2016-09-13 10:39:50 -0700

    lol

---
+++ APQuiz_01/StudyGuide.java
@@ -0,0 +1,26 @@
+public class StudyGuide{
+	public static void main(String[]args){
+		//Question_01
+		int num = 45;
+		System.out.println("IronMan has " + num + " kinds of weapons in his suit");
+		
+		//Question_02
+		String pres = "Abraham Lincoln";
+		String quote = "Whatever you are, be a good one.";
+		System.out.println(pres + " once said, \"" + quote +  "\"");
+		
+		//Question_03
+		int h = 45;
+		int w = 64;
+		System.out.println("The area of your rectangle is..." + h*w);
+		
+		//Question_04
+		/*
+		System.out.println("How many pieces of pizza do you want? ");
+		int slices = kb.nextInt();
+		System.out.println(slices + "slices, coming right up!");
+		
+		Produces an error because "seven" is a String, and slices is an
+		*/
+	}
+}
