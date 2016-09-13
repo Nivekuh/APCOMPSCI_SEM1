2ff2ae4e45c8a47ea4be3e3cfa83687a75b759df
Nivekuh <kevin.hu1000@gmail.com>
2016-09-13 11:02:55 -0700

    Updated Files

--- APQuiz_01/StudyGuide.java
+++ APQuiz_01/StudyGuide.java
@@ -20,7 +20,33 @@ public class StudyGuide{
 		int slices = kb.nextInt();
 		System.out.println(slices + "slices, coming right up!");
 		
-		Produces an error because "seven" is a String, and slices is an
+		Produces an error because "seven" is a String, and variable "slices" is an int.
+		*/
+		
+		//Question_05
+		//C
+		
+		//Question_06
+		//A
+		
+		//Question_07
+		/*
+		System.out.println("R. Kelly said "I believe I can fly!"");
+		
+		Produces an error because the quotations around "I belive I can fly" end the String.
+		You can fiz this by putting a backslash before the two quotation marks.
+		*/
+		
+		//Question_08
+		System.out.println("This one goes on top\nThis one goes on bottom");
+		
+		//Question_09
+		System.out.println("a  b  c\nd  e  f\ng  h  i");
+		
+		//Question_10
+		/*The program produces 0 because all the datatypes are integers, which cannot have demical values.
+		Since the gpa he calculates comes out to be a decimal less than 1, because it's an integer, it just cuts off the decimal and adopts the value of 0.
+		You can fiz this by using doubles instead of ints.
 		*/
 	}
 }
