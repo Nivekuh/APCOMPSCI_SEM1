50b1e70a3f7370b9e81554d2059cb300fcc8ec33
Nivekuh <kevin.hu1000@gmail.com>
2017-02-07 11:08:09 -0800

    Just in case

--- APLesson_13/Ex_01/ToyRunner.java
+++ APLesson_13/Ex_01/ToyRunner.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class ToyRunner{
 	static Scanner kb = new Scanner(System.in);
 	static String toys = "";
 	
 	public static void main(String[]args){
+		/*Car car = new Car("Prius");
+		AFigure figure = new AFigure("Peter's waifu");
+		
+		System.out.println(car + " " + figure);*/
+		
 		/*while(true){
 			System.out.println("Would you like to enter a new toy? (Y or N)");
 			if(kb.next().toLowerCase().equals("y"))
@@ -14,13 +18,14 @@ public class ToyRunner{
 			else
 				break;
 		}*/
+		
 		toys = "Hotwheel, Car, G.I.Joe, AF, PennyRacer, Car, Matchbox, Car, Star Wars, AF, Pullback, Car, Star Wars, AF";
 		
 		ToyStore store = new ToyStore(toys);
 		
 		System.out.println(store);
-		
-		
+		System.out.println("Most common Toy: " + store.getMostFrequentToy());
+		System.out.println("Most common Type: " + store.getMostFrequentType());
 	}
 	
 	static void promptToy(){
