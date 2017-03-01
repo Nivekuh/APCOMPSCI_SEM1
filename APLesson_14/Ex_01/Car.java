bc479f59bdf9dbd208d2ed324e9607352c59ffbe
Nivekuh <kevin.hu1000@gmail.com>
2017-03-01 11:45:58 -0800

    14

---
+++ APLesson_14/Ex_01/Car.java
@@ -0,0 +1,20 @@
+public class Car implements Location{
+	private double[] loc;
+	private int ID;
+	
+	public Car(){
+		ID = (int)(Math.random() * (Math.pow(10, 6) - 1) + 1); //1 to 999999
+	}
+	
+	public double[] getLoc(){
+		return loc;
+	}
+	
+	public int getID(){
+		return ID;
+	}
+	
+	public void move(double x, double y){
+		loc = new double[] {x, y};
+	}
+}
---
+++ APLesson_14/Ex_01/GMC.java
@@ -0,0 +1,5 @@
+public class GMC extends Car{
+	public GMC(double x, double y){
+		this.move(x, y);
+	}
+}
