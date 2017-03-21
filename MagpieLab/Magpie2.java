89d52ed68ad19c3bf13e909fee8b22a603b77f01
Nivekuh <kevin.hu1000@gmail.com>
2017-03-21 12:05:41 -0700

    Magpies

---
+++ MagpieLab/Magpie2.java
@@ -0,0 +1,101 @@
+public class Magpie2
+{
+	/** Get a default greeting @return a greeting*/
+	public String getGreeting()
+	{
+		return "Hello, let's talk.";
+	}
+	
+	/** getResponse() method
+	 * ===========================================
+	 * 	Gives a response to a user statement
+	 *  @param statement (the user statement)
+	 * 	@return a response based on the rules given */
+	public String getResponse(String statement)
+	{
+		String response = "";
+		 
+		if(statement.length() == 0){
+			response = "Say something, please.";
+		}
+
+		else if (findKeyword(statement, "no") >= 0)
+		{
+			response = "Why so negative?";
+		}
+
+		else if (findKeyword(statement, "mother") >= 0
+				|| findKeyword(statement, "father") >= 0
+				|| findKeyword(statement, "sister") >= 0
+				|| findKeyword(statement, "brother") >= 0)
+		{
+			response = "Tell me more about your family.";
+		}
+		
+		else if (findKeyword(statement, "cat") >= 0
+				|| findKeyword(statement, "dog") >= 0
+				|| findKeyword(statement, "fish") >= 0
+				|| findKeyword(statement, "turtle") >= 0)
+		{
+			response = "Tell me more about your pet.";
+		}
+		
+		else if (findKeyword(statement, "robinette") >= 0)
+		{
+			response = "He sounds like a pretty dank teacher.";
+		}
+
+		else
+		{
+			response = getRandomResponse();
+		}
+		return response;
+	}
+
+	/** Ex_02: The findKeyword() Method...
+	 * ========================================================= */
+	private boolean isLetter(String c){
+		return (c.compareTo("a") >= 0 && c.compareTo("a") <= 25);
+	}
+	 
+	private int findKeyword(String statement, String goal, int startPos)
+	{
+		String phrase = " " + statement.trim().toLowerCase() + " ";
+		int psn = phrase.indexOf(goal, startPos);
+		
+		if(psn != -1 && (isLetter(phrase.substring(psn - 1, psn)) || isLetter(phrase.substring(psn + goal.length(), psn + goal.length() + 1))))
+			return findKeyword(statement, goal, psn + 1);
+		
+		return psn;
+	}
+
+	/** Override - this method is used if there are only 2 parameters...*/
+	private int findKeyword(String statement, String goal)
+	{
+		//set startPos to 0 if not specified
+		return findKeyword(statement, goal, 0);
+	}
+
+	/** getRandomResponse() method
+	 * =============================================================*/
+	/** Pick a default response to use if nothing else fits.
+	 * 	@return a non-committal string*/
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 4;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
+			response = "Interesting, tell me more.";
+		else if (whichResponse == 1)
+			response = "Hmmm.";
+		else if (whichResponse == 2)
+			response = "Do you really think so?";
+		else if (whichResponse == 3)
+			response = "You don't say.";
+
+		return response;
+	}
+}
